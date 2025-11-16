0248bf35f3d1fc4cd5db57881b27128b2d9a785c
Kaitlin Wan <33489138+KaitlinWan@users.noreply.github.com>
2025-11-16 00:55:53 -0500

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://KaitlinWan.github.io/me',
-  base: '/',
+  base: '/me',
 });
